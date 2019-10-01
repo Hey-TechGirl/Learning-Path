5d21f49c972761d9a21a730183e7a5473d22611c
Hey-techgirl <46885944+Hey-TechGirl@users.noreply.github.com>
2019-10-01 08:27:40 +0300

    Update 03_07.py

--- Notebook/Become a Front-End Web Developer/3-programming-foundation-exercises/chap03/03_07.py
+++ Notebook/Become a Front-End Web Developer/3-programming-foundation-exercises/chap03/03_07.py
@@ -3,12 +3,17 @@ print("Challenge 1:")
 # A message for the user
 message = "This is going to be tricky ;)"
 Message = "Very tricky!"
-print(message) # show the message on the screen "This is going to be tricky ;)"
+print(message) # show the message on the screen
 
 # Perform mathematical operations
 result = 2**3
-print("2**3 =", result) # result = 8
+print("2**3 =", result) 
 result = 5 - 3
-#print("5 - 3 =", result) # result = 2
+#print("5 - 3 =", result) 
 
 print("Challenge complete!")
+
+### the code will print
+# This is going to be tricky ;)
+# 2 ** 3 = 8
+# Challenge complete!
